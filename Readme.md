295fe5ca321b7c25c1f45879125eea907d8a8b55
Dali Kilani <dkilani@zynga.com>
2011-12-04 14:39:12 -0800

    First iteration of a result scraper for the Tunisian Elections result

---
+++ Readme.md
@@ -0,0 +1,23 @@
+Intro
+-----
+
+This is a simple scrip to extract the Tunisian Constitutional Assembly detailed results from the ISIE website
+
+Pre-requisites
+--------------
+- Python 2.6+
+
+Install BeautifulSoup
+---------------------
+Unpack BeautifulSoup.tar.gz into a directory
+Run inside the BeautifulSoup directory :
+     python setup.py build
+     python setup.py install
+
+Run the script
+--------------
+./isie_scrape.py
+
+Result
+------
+- Election_results.csv
---
+++ isie_scrape.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+from BeautifulSoup import BeautifulSoup
+import re
+import urllib2
+from urllib import urlencode
+
+urlResult='http://www.isie.tn/Ar/rsfinal.php'
+urlDelegation='http://www.isie.tn/Ar/ws_delegation_ajax.php'
+urlCentre='http://www.isie.tn/Ar/ws_centre_vote_ajax.php'
+urlBureau='http://www.isie.tn/Ar/ws_bureau_vote_ajax.php'
+
+def getCirc():
+    params = {
+    }
+    encoded_params = urlencode(params)
+    
+    res = urllib2.urlopen(urlResult, encoded_params)
+    soup = BeautifulSoup(''.join(res.read()))
+    optList = soup.find("select",{ 'id' : 'circonscription'})
+    optTags = optList.findAll('option')[1:]
+    return map(lambda x: {x['value'] : x.contents[0]}, optTags)
+    
+def getDelegation(circonscription):
+    params = {
+        'circonscription' : circonscription
+    }
+    encoded_params = urlencode(params)
+    
+    res = urllib2.urlopen(urlDelegation, encoded_params)
+    soup = BeautifulSoup(''.join(res.read()))
+    optTags = soup.findAll('option')[1:]
+    return map(lambda x: {x['value'] : x.contents[0]}, optTags)
+
+def getCentre(circonscription, delegation):
+    params = {
+        'circonscription' : circonscription,
+        'delegation':delegation,
+    }
+    encoded_params = urlencode(params)
+    
+    res = urllib2.urlopen(urlCentre, encoded_params)
+    soup = BeautifulSoup(''.join(res.read()))
+    optTags = soup.findAll('option')[1:]
+    return map(lambda x: {x['value'] : x.contents[0]}, optTags)
+
+def getBureau(circonscription, delegation, centre):
+    params = {
+        'circonscription' : circonscription,
+        'delegation':delegation,
+        'centre_vote':centre,
+    }
+    encoded_params = urlencode(params)
+    
+    res = urllib2.urlopen(urlBureau, encoded_params)
+    soup = BeautifulSoup(''.join(res.read()))
+    optTags = soup.findAll('option')[1:]
+    return map(lambda x: {x['value'] : x.contents[0]}, optTags)
+
+# create the request object and set some headers
+def getResult(circonscription, delegation, centre, bureau):
+    params = {
+        'circonscription':circonscription,
+        'delegation':delegation,
+        'centre_vote':centre,
+        'bureau_vote':bureau,
+        'button':'+++%D8%A8%D8%AD%D8%AB++',
+    }
+    encoded_params = urlencode(params)
+    
+    res = urllib2.urlopen(urlResult, encoded_params)
+    soup = BeautifulSoup(''.join(res.read()))
+    optTags = soup.findAll('option')[1:]
+    return map(lambda x: {x['value'] : x.contents[0]}, optTags)
+    return res.read()
+
+print getCirc()
+print getDelegation('130')
+print getCentre('130', '61')
+print getBureau('130', '61', '091')
+
