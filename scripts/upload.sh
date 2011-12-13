e203fe849e719aee5ba38ee1f3b3ab8b9a69920c
Dali Kilani <dkilani@zynga.com>
2011-12-13 00:03:30 -0800

    Added the database name as a parameter to the script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,8 +2,14 @@
 
 # This script loads the patched json files into a local CouchDB
 
+if [ $# -ne  1 ];
+then
+  echo "Usage : upload.sh DATABASE_NAME"
+fi
+
+DB_NAME=$1
 for f in raw-*.json
 do
 CONTENT=`cat $f`
-curl -X POST "http://127.0.0.1:5984/isie_election_final/" -H 'Content-Type: application/json' -d "$CONTENT"
+curl -X POST "http://127.0.0.1:5984/$DB_NAME/" -H 'Content-Type: application/json' -d "$CONTENT"
 done
